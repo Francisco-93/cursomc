c472f8ed5585e10e18522ec16682cb30ae2d3f9c
Francisco-93 <francisco.fro.agui@gmail.com>
2021-03-08 23:37:39 -0300

    endpoint post Categoria disponibilizado

--- src/main/java/com/franciscoaguiar/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/franciscoaguiar/cursomc/resources/CategoriaResource.java
@@ -1,12 +1,17 @@
 package com.franciscoaguiar.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.franciscoaguiar.cursomc.domain.Categoria;
 import com.franciscoaguiar.cursomc.services.CategoriaService;
@@ -23,6 +28,13 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> findById(@PathVariable Integer id){
 		Categoria obj = service.buscar(id);
 		return new ResponseEntity<Categoria>(obj, HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
