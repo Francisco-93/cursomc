22130d031c15add4e02576e09b4c0dd1a956bc41
Francisco-93 <francisco.fro.agui@gmail.com>
2021-03-11 23:46:59 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/franciscoaguiar/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/franciscoaguiar/cursomc/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -63,6 +65,18 @@ public class CategoriaResource {
 		List<CategoriaDTO> listDTO = list.stream().map(cat -> new CategoriaDTO(cat)).collect(Collectors.toList());		
 		
 		return new ResponseEntity<List<CategoriaDTO>>(listDTO, HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDTO = list.map(cat -> new CategoriaDTO(cat));		
+		
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 	private Categoria DTOParseObject(CategoriaDTO catDTO) {
